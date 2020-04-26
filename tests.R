743ecf9e559deda5b8e8f8fafd9cd04289590bce
bambooforest@gmail.com <bambooforest@gmail.com>
2020-04-26 12:58:36 +0200

    identify mismatches

--- tests.R
+++ tests.R
@@ -1,19 +1,30 @@
 library(dplyr)
+library(testthat)
 library(knitr)
 
 # wwsd tables
-bib <- read.csv('bibliography.csv', sep="\t")
-db <- read.csv('database.csv')
+bib <- read.csv('bibliography.csv', sep="\t", stringsAsFactors = FALSE)
+db <- read.csv('database.csv', stringsAsFactors = FALSE)
+
+db <- db %>% arrange(script)
+rownames(db) <- NULL
+
+bib <- bib %>% arrange(script)
+rownames(bib) <- NULL
 
 # Which identifiers don't match?
+db.s <- db %>% select(script) %>% distinct()
+bib.s <- bib %>% select(script) %>% distinct()
 
 ## Entries in the database not in the bibliography index
-kable(db[which(!(unique(db$script) %in% unique(bib$script))),])
+expect_true(all(db.s$script %in% bib.s$script))
+kable(db.s[which(!(db.s$script %in% bib.s$script)),])
 
 ## Entries in the bibliography not in the database
-kable(bib[which(!(unique(bib$script) %in% unique(db$script))),])
+expect_true(all(bib.s$script %in% db.s$script))
+kable(bib.s[which(!(bib.s$script %in% db.s$script)),])
 
-# Which urls don't match
+# Which urls don't match (some of these are discrepencies between http and https)
 db.url <- db %>% select(omniglot) %>% distinct()
 bib.url <- bib %>% select(omniglot) %>% distinct()
 
