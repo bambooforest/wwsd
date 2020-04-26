46c50c3ac1aec3df13fb4d0b0e39e2db9d8eda0b
bambooforest@gmail.com <bambooforest@gmail.com>
2020-04-26 12:23:18 +0200

    get unique urls

--- tests.R
+++ tests.R
@@ -13,6 +13,9 @@ kable(db[which(!(unique(db$script) %in% unique(bib$script))),])
 ## Entries in the bibliography not in the database
 kable(bib[which(!(unique(bib$script) %in% unique(db$script))),])
 
-# Which urls don't match?
+# Which urls don't match
+db.url <- db %>% select(omniglot) %>% distinct()
+bib.url <- bib %>% select(omniglot) %>% distinct()
+
 bib.url[which(!(unique(bib$omniglot) %in% unique(db$omniglot))),]
-db.url[which(!(unique(db$omniglot) %in% unique(bib$omniglot))),]
+db.url[which(!(db.url$omniglot %in% bib.url$omniglot)),]
