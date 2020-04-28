59a7760a423548fbbb30d4eaf66701e91e41b1e6
bambooforest@gmail.com <bambooforest@gmail.com>
2020-04-28 11:56:10 +0200

    add update url code

--- tests.R
+++ tests.R
@@ -6,7 +6,7 @@ library(knitr)
 bib <- read.csv('bibliography.csv', sep="\t", stringsAsFactors = FALSE)
 db <- read.csv('database.csv', stringsAsFactors = FALSE)
 
-# order for the results
+# Ordered for results
 db <- db %>% arrange(script); rownames(db) <- NULL
 bib <- bib %>% arrange(script); rownames(bib) <- NULL
 
@@ -14,19 +14,32 @@ bib <- bib %>% arrange(script); rownames(bib) <- NULL
 db.s <- db %>% select(script) %>% distinct()
 bib.s <- bib %>% select(script) %>% distinct()
 
-## Entries in the database not in the bibliography index
+## Entries in the database that are not in the bibliography index
 expect_true(all(db.s$script %in% bib.s$script))
 kable(db.s[which(!(db.s$script %in% bib.s$script)),])
 
-## Entries in the bibliography not in the database
+## Entries in the bibliography that are not in the database
 expect_true(all(bib.s$script %in% db.s$script))
 kable(bib.s[which(!(bib.s$script %in% db.s$script)),])
 
 # Which urls don't match (some of these are discrepencies between http and https, others have an extra space)
-# TODO: fix these
 db.url <- db %>% select(omniglot) %>% distinct()
 bib.url <- bib %>% select(omniglot) %>% distinct()
 
 db.url[which(!(db.url$omniglot %in% bib.url$omniglot)),]
 bib.url[which(!(bib.url$omniglot %in% db.url$omniglot)),]
+
+# Recreate the database.csv file with the updated (omniglot) fields in the bibliography.csv
+df <- left_join(db, bib, by=c("script"="script"))
+df <- df %>% select(script, grapheme, IPA, comment, source.x, omniglot.y)
+df <- df %>% rename(source=source.x, omniglot=omniglot.y)
+## Check the results of the omniglot url updates
+scripts.urls <- df %>% select(script, omniglot) %>% distinct() %>% arrange(script)
+distinct.scripts.in.db <- db %>% select(script) %>% distinct()
+expect_true(nrow(distinct.scripts.in.db)==nrow(scripts.urls))
+## For visual inspection
+write.csv(scripts.urls, file="scripts_urls.csv", row.names = FALSE)
+## Write new file
+write.csv(df, file="database.csv", row.names = FALSE, quote=FALSE)
+
 
